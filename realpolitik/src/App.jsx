5790abf46d08506f77e36e64f0dbac27ba86f06f
colecrum26 <cwcrumrine@gmail.com>
2024-04-09 13:34:31 -0500

    built out question framework

--- realpolitik/src/App.jsx
+++ realpolitik/src/App.jsx
@@ -1,17 +1,55 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    <h1>Society</h1>
-    <h1>Economy</h1>
-    <h1>Liberty</h1>
-    <h1>Nationalism</h1>
-    <p></p>
-    </>
-  )
+    <div className="quiz-container">
+      <div className="society">
+        <h1 className="q-header">Society</h1>
+        <div className="q-wrapper">
+          <p className="question"></p>
+          <button className="dis-str">Strongly Disagree</button>
+          <button className="dis">Disagree</button>
+          <button className="neu">Neutral</button>
+          <button className="agr">Agree</button>
+          <button className="agr-str">Strongly Agree</button>
+        </div>
+      </div>
+      <div className="economy">
+        <h1 className="question-header">Economy</h1>
+        <div className="q-wrapper">
+          <p className="question"></p>
+          <button className="dis-str">Strongly Disagree</button>
+          <button className="dis">Disagree</button>
+          <button className="neu">Neutral</button>
+          <button className="agr">Agree</button>
+          <button className="agr-str">Strongly Agree</button>
+        </div>
+      </div>
+      <div className="liberty">
+        <h1 className="question-header">Liberty</h1>
+        <div className="q-wrapper">
+          <p className="question"></p>
+          <button className="dis-str">Strongly Disagree</button>
+          <button className="dis">Disagree</button>
+          <button className="neu">Neutral</button>
+          <button className="agr">Agree</button>
+          <button className="agr-str">Strongly Agree</button>
+        </div>
+      </div>
+      <div className="nationalism">
+        <h1 className="question-header">Nationalism</h1>
+        <div className="q-wrapper">
+          <p className="question"></p>
+          <button className="dis-str">Strongly Disagree</button>
+          <button className="dis">Disagree</button>
+          <button className="neu">Neutral</button>
+          <button className="agr">Agree</button>
+          <button className="agr-str">Strongly Agree</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
